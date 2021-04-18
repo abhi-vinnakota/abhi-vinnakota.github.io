bde503394aa9b75157654a45c53ab0be890951aa
Abhi Vinnakota <abhi.vinnakota5@gmail.com>
2021-04-17 23:56:45 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    var typed = new Typed(".typing", {
+        strings: ["Student", "Programmer", "Researcher", "Filmmaker"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+        
+    });
+
+    
+});
