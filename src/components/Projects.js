a441fb146d693d70f06f9dc32a7470a7b11e66fd
Abhi Vinnakota <abhi.vinnakota5@gmail.com>
2022-01-01 18:48:07 -0600

    Reworking directory + Changes to Intro and About Sections

---
+++ src/components/Projects.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../css/Projects.css'
+
+class Projects extends React.Component {
+    render() {
+        return <h2>Projects</h2>;
+    }
+}
+
+export default Projects;
